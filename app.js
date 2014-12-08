e4ed00e1f93b96bc7a6f87130c5c2f2e5a97d654
TheLocust07 <akebra@gmail.com>
2014-12-08 19:40:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -343,6 +343,8 @@ global.Simulator = require('./simulator.js');
 
 global.Tournaments = require('./tournaments');
 
+global.DatabaseManager = require('./db_manager.js');
+
 try {
 	global.Dnsbl = require('./dnsbl.js');
 } catch (e) {
